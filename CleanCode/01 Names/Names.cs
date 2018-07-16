5ff4b5bebdac194c3373087a02c5965a7343fbce
Jesus Caceres <jesus.caceres85@hotmail.com>
2018-07-16 17:52:26 -0500

    updating variables name and method according to Clean Code conventions

--- CleanCode/01 Names/Names.cs
+++ CleanCode/01 Names/Names.cs
@@ -4,14 +4,14 @@ namespace CleanCode.Names
 {
     public class Names
     {
-        public Bitmap Method1(string n)
+        public Bitmap CreateGraphicFromBitmap(string fileName)
         {
-            var b = new Bitmap(n);
-            var g = Graphics.FromImage(b);
-            g.DrawString("a", SystemFonts.DefaultFont, SystemBrushes.Desktop, new PointF(0, 0));
-            g.DrawString("b", SystemFonts.DefaultFont, SystemBrushes.Desktop, new PointF(0, 20));
-            g.DrawString("c", SystemFonts.DefaultFont, SystemBrushes.Desktop, new PointF(0, 30));
-            return b;
+            var bitmap = new Bitmap(fileName);
+            var canvas = Graphics.FromImage(bitmap);
+            canvas.DrawString("a", SystemFonts.DefaultFont, SystemBrushes.Desktop, new PointF(0, 0));
+            canvas.DrawString("b", SystemFonts.DefaultFont, SystemBrushes.Desktop, new PointF(0, 20));
+            canvas.DrawString("c", SystemFonts.DefaultFont, SystemBrushes.Desktop, new PointF(0, 30));
+            return bitmap;
         }
     }
 }
